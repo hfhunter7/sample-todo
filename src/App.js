d1556d9512eaa89c93feea28620e7255c2f7f79f
wiwat <wiwat@9picompany.com>
2018-06-28 11:56:45 +0700

    test filter

--- src/App.js
+++ src/App.js
@@ -10,13 +10,20 @@ class App extends Component {
 		this.state = {
 			title: 'Welcome to my app',
 			items: ['hello','world'],
+			numbers: [1,2,3,4,5,6,7,8,9,10]
 		}
 	}
 
 	addTodo = (newTodo) => {
-		this.setState({
-			items: this.state.items.concat([newTodo])
+		// this.setState({
+		// 	items: this.state.items.concat([newTodo])
+		// })
+
+		const number = this.state.numbers.filter((data) => {
+			return data % 2 === 0
 		})
+
+		console.log(number)
 	}
 
 	deleteTodo = (index) => {
